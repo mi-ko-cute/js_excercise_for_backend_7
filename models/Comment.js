ff1b115a3a63971612d15ec22c63f17798238812
mi-ko-cute <thx-mail@worysolve.com>
2019-11-24 18:56:17 +0900

    Comment Modelの用意

---
+++ models/Comment.js
@@ -0,0 +1,23 @@
+const comments = [];
+
+let nextId = 1;
+
+class Comment {
+    constructor({ username, body }) {
+        this.id = nextId++;
+        this.username = username;
+        this.body = body;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+}
+
+// Getでコメント一覧が返ってくる事を確かめるためにダミーデータを用意
+for (let i = 1; i <= 5; i++) {
+    const comment = new Comment({
+        username: 'ユーザネーム' + i,
+        body: '詳細文' + i
+    })
+
+    comments.push(comment);
+}
