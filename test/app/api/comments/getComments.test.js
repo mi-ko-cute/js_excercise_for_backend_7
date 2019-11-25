0fa6451b63706ff67feead92000e28e3962fed0c
mi-ko-cute <thx-mail@worysolve.com>
2019-11-25 13:37:36 +0900

    GET /api/comments」にリクエストを投げたらコメント一覧が返ってくるAPIのテストを行う

---
+++ test/app/api/comments/getComments.test.js
@@ -0,0 +1,22 @@
+const assert = require('power-assert');
+const requestHelper = require('../../../helper/requestHelper');
+
+describe('test 「GET /api/comments」', () => {
+    it('returns comments in resopnse.body', async () => {
+        const response = await requestHelper.request({
+            method: 'get',
+            endPoint: '/api/comments',
+            statusCode: 200
+        });
+
+        const comments = response.body;
+        assert.ok(Array.isArray(comments));
+        todos.forEach((comment) => {
+            assert.strictEqual(typeof comment.id, 'number');
+            assert.strictEqual(typeof comment.username, 'string');
+            assert.strictEqual(typeof comment.body, 'string');
+            assert.strictEqual(typeof comment.createdAt, 'string');
+            assert.strictEqual(typeof comment.updatedAt, 'string');
+        });
+    });
+}); 
